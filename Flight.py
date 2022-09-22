8fc0e17d0dfa325bb397e0662bc550caf4e58c80
Jagoda Tata <sc20jmt@leeds.ac.uk>
2022-09-22 13:44:42 +0200

    removing hard-coded destination city

--- Flight.py
+++ Flight.py
@@ -7,9 +7,9 @@ class Flight:
     __departure_country = None
     __departure_city = None
     __depart_port_IATA_code = None
-    __destination_country = 'Wielka Brytania'
-    __destination_city = 'Leeds'
-    __dest_port_IATA_code = 'LBA'
+    __destination_country = None
+    __destination_city = None
+    __dest_port_IATA_code = None
     __date = None
     __price = -1
     __price_currency = None
@@ -21,7 +21,10 @@ class Flight:
         self.__departure_city = depart_city
         self.__departure_country = Airports[depart_city].country
         self.__depart_IATA_code = Airports[depart_city].IATA_code
+
         self.__destination_city = dest_city
+        self.__destination_country = Airports[dest_city].country
+        self.__depart_IATA_code = Airports[dest_city].IATA_code
         self.__price_currency = Airports[depart_city].currency
 
     def setPrice(self, price):
