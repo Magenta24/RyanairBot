0b77d23010116717d4b359971e29e3b9ae855e56
Jagoda Tata <sc20jmt@leeds.ac.uk>
2022-10-01 16:18:50 +0200

    main

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
 import threading
 import time
 from RyanairBot import RyanairBot
+from GUI.RyanairGUI import RyanairGUI
 
 
 departure_cities = ('Krakow', 'Wroclaw', 'Poznan')
 
-bot_krk = RyanairBot('Chrome', 'Krakow', 'Leeds')
-botek_poz = RyanairBot('Chrome', 'Poznan', 'Leeds')
-botek_wro = RyanairBot('Chrome', 'Wroclaw', 'Leeds')
+ryanairbot = RyanairGUI(RyanairBot)
 
-t_krk = threading.Thread(target=bot_krk.run())
-t_poz = threading.Thread(target=botek_poz.run())
-t_wro = threading.Thread(target=botek_wro.run())
-
-t_krk.start()
-t_wro.start()
-t_poz.start()
 
